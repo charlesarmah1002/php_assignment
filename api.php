38f09705c4d614d85f9133e6bb1d13b24f95a7aa
Charles Wesley Armah <116085774+charlesarmah1002@users.noreply.github.com>
2023-11-30 10:15:59 +0000

    using xhr for requests to api.php

--- api.php
+++ api.php
@@ -1,21 +1,77 @@
 <?php
-// phpinfo();
-$hostname = 'localhost:3306';
-$username = 'root';
-$password = 'development';
-$database = 'php_assignment';
 
-// Create a MySQLi connection
-$mysqli = new mysqli($hostname, $username, $password, $database);
+session_start();
 
-// Check connection
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $mysqli->connect_error);
+
+
+// variable declarations
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $endpoint = rtrim($_SERVER['REQUEST_URI'], '/');
+
+    $endpoints = [
+        '/api.php/register' => 'handleRegistration',
+        '/api.php/login' => 'handleLogin',
+        '/api.php/status' => 'userStatus',
+    ];
+
+    if (isset($endpoints[$endpoint])) {
+        $handler = $endpoints[$endpoint];
+        $handler();
+    } else {
+        http_response_code(404);
+        echo json_encode(['error' => 'Endpoint not found']);
+    }
+} else {
+    http_response_code(405);
+    echo 'Method Not Allowed';
 }
 
-echo "Connected successfully";
+function handleRegistration()
+{
+    $hostname = "localhost:3306";
+    $username = "root";
+    $password = "development";
+    $dbname = "php_assignment";
+    
+    $conn = mysqli_connect($hostname, $username, $password, $dbname);
+    if (!$conn) {
+        echo "Database connection error" . mysqli_connect_error();
+    }
+    
+    $username = mysqli_escape_string($conn, $_POST['username']);
+    $password = mysqli_escape_string($conn, $_POST['password']);
 
-// ... (perform database operations)
+    if(!empty($username) && !empty($password)) {
+        $query1 = mysqli_query($conn, "SELECT * FROM users WHERE username = '{$username}'");
+        if(mysqli_num_rows($query1) > 0){
+            echo "User Already Exists";
+        }else {
+            $unique_id = rand(100000, 999999);
 
-// Close the connection when done
-$mysqli->close();
+            $enc_pass = password_hash($password, PASSWORD_DEFAULT);
+
+            $query2 = mysqli_query($conn, "INSERT INTO users(unique_id, username, password) VALUES({$unique_id}, '{$username}', '{$enc_pass}')");
+
+            if($query2){
+                $_SESSION['unique_id'] = $unique_id;
+                echo "success";
+            }
+        }
+    }else {
+        echo "Invalid or empty string";
+    }
+}
+
+function handleLogin()
+{
+    // Implement logic for user login
+    // ...
+
+    // Respond with JSON data
+    // echo json_encode(['message' => 'Login successful']);
+}
+
+// Add more functions for other API endpoints as needed
--- script.js
+++ script.js
@@ -1,15 +1,46 @@
-const form = document.getElementById("form"),
-    registerBtn = form.querySelector("button.register"),
-    signUpBtn = form.querySelector("button.signup");
+const form = document.querySelector('form'),
+    registerBtn = form.querySelector('button.register'),
+    signInBtn = form.querySelector('button.signin');
+
 
 form.onsubmit = (e) => {
     e.preventDefault();
 }
 
 registerBtn.onclick = () => {
-    window.alert("this is the regigster button");
+    console.log('this is the register button');
+
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("POST", "api.php/register", true)
+
+    xhr.onload = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                let data = xhr.response;
+                if (data === "success") {
+
+                } else {
+                    console.log(data);
+                }
+            }
+        }
+    };
+
+    // Set up a callback for network errors
+    xhr.onerror = function () {
+        console.log("Network Error");
+    };
+
+    let formData = new FormData(form);
+    // Send the request
+    xhr.send(formData);
 }
 
-signUpBtn.onclick = () => {
-    window.alert("this is the sign up button");
+signInBtn.onclick = () => {
+    console.log('this is the sign in button')
+}
+
+function checkStatus() {
+    
 }
